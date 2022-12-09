d6c32c24d1710fff9588130413a945ab63bc178d
daafne <dafne.serrano@eaceuta.com>
2022-12-09 09:38:30 +0100

    añado silo y tarjetasresponsive
    nuevo :)

--- SILO/js/estilo.js
+++ SILO/js/estilo.js
@@ -10,39 +10,42 @@
 
 
 const icono_burger = document.getElementById("burger");
-// console.log(icono_burger);
+console.log(burger);
 
 const menu = document.querySelector(".menu");
+console.log(menu);
+
+//hago que al click salga el menu, aplicando estas sentancias, 
+
 
 icono_burger.addEventListener("click", 
       function(){
 
-      // console.log(menu.classList);
-       // menu.classList.add("nuevo")
-       // menu.classList.remove("menu")
-       // console.log(menu.classList);
-       //  console.log(menu.classList.contains("nuevo"));
 
-       if (menu.classList.contains("ocultar") == true){
-         menu.classList.remove("ocultar")
-         menu.classList.add("mostrar")
-       }
-       else if (menu.classList.contains("mostrar") == true){
-           menu.classList.remove("mostrar")
-           menu.classList.add("ocultar")
-       }
+      // estas lineas se pueden sustituir utilizando el metodo toggle
 
-       // div.classList.replace("ocultar", "mostrar");    
-       // div.classList.replace("mostrar", "ocultar"); 
+      menu.classList.toggle("mostrar");
+
+       /*
+       if (menu.classList.contains("mostrar") == true){
+          menu.classList.remove("mostrar");
+       }
+       else {
+          menu.classList.add("mostrar");
+       }
+       */
+
      });
+
+
 //recupero el elemento html que hace referencia al corazon
-const corazon= document.querySelector(".corazon");
-console.log(corazon);
+const corazon= document.querySelector(".heart");
+console.log(heart);
 
-corazon.addEventListener("click",function)(){
-this.
+//corazon.addEventListener("click",function)(){
+//this.
 
-});
+//});
      
      
 
